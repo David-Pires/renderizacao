b6bdd89d5e3d89f72c7acd42edca5b5af3835b44
David Pires <developer.david@outlook.com>
2022-03-02 11:13:02 -0300

    dinamic 2

--- pages/api/produtos.js
+++ pages/api/produtos.js
@@ -1,10 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+function numeroAleatorio(min = 1, max = 100000) {
+  return parseInt(Math.random() * (max - min)) + min
 
+}
 export default function handler(req, res) {
   res.status(200).json([
-    {id: 1, nome: 'Caneta', preco: 5.60},
-    {id: 2, nome: 'Lapis', preco: 2.60},
-    {id: 3, nome: 'Caderno', preco: 16.60},
-    {id: 4, nome: 'Borracha', preco: 2.00}
+    {id: numeroAleatorio(), nome: 'Caneta', preco: 5.60},
+    {id: numeroAleatorio(), nome: 'Lapis', preco: 2.60},
+    {id: numeroAleatorio(), nome: 'Caderno', preco: 16.60},
+    {id: numeroAleatorio(), nome: 'Borracha', preco: 2.00}
   ])
 }
