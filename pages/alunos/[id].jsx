b6aeaa6db05b9361efa98f4187ac74b2e7ca3f46
David Pires <developer.david@outlook.com>
2022-03-03 11:23:26 -0300

    getStaticPaths 2second

--- pages/alunos/[id].jsx
+++ pages/alunos/[id].jsx
@@ -1,25 +1,38 @@
-export function getStaticPaths() {
+export async function getStaticPaths() {
+  const resp = await fetch('http://localhost:3000/api/alunos/tutores ')
+  const ids = await resp.json()
+
+  const paths = ids.map(id => {
+    return { params: { id: `${id}` }}
+  })
+
+
   return {
     fallback: false, // gera erro 404
-    paths: [
-      { params: { id: '1'}},
-      { params: { id: '2'}},
-      { params: { id: '3'}},
-      
-    ]
+    paths        
   }
 }
 
-export function getStaticProps() {
+export async function getStaticProps({ params }) {
+  const resp = await fetch('http://localhost:3000/api/alunos/${params.id}')
+  const aluno = await resp.json()
   return {
-    props: {}
+    props: {
+      aluno
+    }
   }
 }
 //geração de conteúdo estático sem conteúdo fixo
-export default function AlunoPorId() {
+export default function AlunoPorId(props) {
+  const { aluno } = props
   return (
     <div>
-      <h1>Detalhes do aluno</h1>      
+      <h1>aluno</h1>   
+      <ul>
+        <li>aluno.id</li>
+        <li>aluno.nome</li>
+        <li>aluno.email</li>
+      </ul>   
     </div>
   )
 }
